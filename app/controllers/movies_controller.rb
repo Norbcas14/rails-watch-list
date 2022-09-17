e17ddc3c713eb0c88ae0711246afc6398e34cecd
norbey bustamante <norbey.bustamante2@gmail.com>
2022-09-17 17:10:06 -0500

    Rails Watch list

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,47 @@
+class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url, :rating)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+end
